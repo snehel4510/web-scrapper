cbb2367bb399ef88f0531b7443ae274f39596d1d
Snehel <sahilrps31@gmail.com>
2022-05-01 21:37:21 +0530

    cors package to fetch data

--- index.js
+++ index.js
@@ -6,18 +6,28 @@ const express = require('express')
 
 const app = express()
 
+const cors = require(cors);
+app.use(cors());
+
 const url = "https://www.theguardian.com/international"
-axios(url)
-    .then(response => {
-        const html = response.data
-        const $ = cheerio.load(html)
-        const titles = []
-        $('.fc-item__container', html).each((i, e) => {
-            const title = $(e).find('a').text()
-            const link = $(e).find('a').attr('href')
-            titles.push({ title, link })
-        })
-        console.log(titles)
-    }).catch(error => { console.log(error) });
+
+app.get('/', function (req, res) {
+    res.json('this is my web-scrapper website')
+})
+
+app.get('/news', (req, res) => {
+    axios(url)
+        .then(response => {
+            const html = response.data
+            const $ = cheerio.load(html)
+            const titles = []
+            $('.fc-item__container', html).each((i, e) => {
+                const title = $(e).find('a').text()
+                const link = $(e).find('a').attr('href')
+                titles.push({ title, link })
+            })
+            res.json(titles)
+        }).catch(error => { console.log(error) });
+})
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
