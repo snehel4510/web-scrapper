eeec59b5b686a0369d63d74726b32140e29434e1
Snehel <sahilrps31@gmail.com>
2022-05-01 21:43:36 +0530

    cors package initialized

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const express = require('express')
 
 const app = express()
 
-const cors = require(cors);
-app.use(cors());
+const cors = require('cors');
+app.use(cors())
 
 const url = "https://www.theguardian.com/international"
 
